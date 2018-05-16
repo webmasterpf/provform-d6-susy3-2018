4ba9d4632851704142f1f44c2baeec8be7a76dc3
webmasterpf-DG <webmaster@provence-formation.com>
2018-05-16 15:47:16 +0200

    Theme PF Susy3 : Node111 : remplacemnt tableau par liste + style, etiquettes en PNG

--- includes/dedicates_inc/inc_objectif_bts.php
+++ includes/dedicates_inc/inc_objectif_bts.php
@@ -1,21 +1,36 @@
 <?php
-
 /*
  * Tableau pour afficher les objectifs BTS (pdf) sans limite 
- * de nombre.
+ * de nombre.Remplacé par liste dynamique pour thème 2018 en 05/2018.
  * Suite conversion ,changement de nom de champ pour les fichiers  : field_fichier_joint_partenaires
  * remplace field_objectif_bts
  */
 ?>
 <?php
-foreach($node->field_fichier_joint_partenaires as $file) {
-  if ($file['view']) {
-      $header = array('data' => 'Les objectifs des formations BTS');
-      $rows[] = array($file['view']);
-      $attributes = array('id'=> 'objectif-bts','sticky' => FALSE);
-    }
-}
+/* 03/2018 Liste les fichiers uploadés par
+ * filefield  création d'une liste dynamique 
+ */
+if (!empty(
+        $node->field_fichier_joint_partenaires[0]['view']
+    )):
+    ?>
+    <div class="objectif-bts">
+    <?php //récupération du nom du champ
+    print $node->content['field_NAME']['field']['#title']
+    ?>
+        <h3>Les objectifs des formations BTS</h3>
+        <ul>
+            <?php
+            //Parcourir le tableau field_NAME de l'objet $node pour lister le contenu clé/valeur
+            foreach ($node->field_fichier_joint_partenaires as $key => $fichiers) {
+                //Ne pas afficher les champs vides du tableau
+                if (!empty($fichiers['view'])):
+//                       print 'Contenu de $fichier'.var_dump($fichier);
 
-$output = theme('table', $header,$rows, $attributes);
-print $output;
-?>
+                    print '<li>' . $node->field_fichier_joint_partenaires[$key]['view'] . '</li>';
+
+                endif;
+            }
+            ?>  
+        </ul>   </div>
+        <?php endif; ?>
