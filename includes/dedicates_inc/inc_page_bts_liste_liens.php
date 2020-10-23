98b2902baadbcaede29443682f41f279707bd8bc
WPF-Local <webmaster@provence-formation.com>
2020-10-23 11:19:18 +0200

    DEV: TdC[21] => page_bts - test classe dynamique nom machine du champs depuis node110

--- includes/dedicates_inc/inc_page_bts_liste_liens.php
+++ includes/dedicates_inc/inc_page_bts_liste_liens.php
@@ -13,7 +13,7 @@
 
                <?php  //récupération du nom du champ si besoin
                print $node->content['field_NAME']['field']['#title'] ?>
-      <ul class="<?php print $node->field_lien_utile_bts;
+      <ul class="<?php print $node->field_lien_utile_bts[0]['view'];
       // Classe dynamique utilisant nom du champ
       ?>">
                <?php
